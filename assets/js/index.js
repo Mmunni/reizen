8fa4ba8644bb896bb0ce8a621ff28493914af804
MasumaMunni <masumamunni20@gmail.com>
2022-08-18 10:48:11 +0600

    footer-added

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+
+
+const openMenu = document.getElementById('open-menu').addEventListener('click', function(){
+    let menu = document.getElementById('nav-menu')
+    menu.style.right = '0';
+    
+    
+   
+    
+});
+const closeMenu = document.getElementById('close-menu').addEventListener('click', function(){
+    let menu = document.getElementById('nav-menu')
+    menu.style.right = '-100vw';
+    
+    
+    
+});
