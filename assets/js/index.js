56ebe6698552ba627d9a6bbce5d5d2b2ed68f709
MasumaMunni <masumamunni20@gmail.com>
2022-09-09 20:06:56 +0600

    navbar- updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,18 @@
 
 const openMenu = document.getElementById('open-menu').addEventListener('click', function(){
     let menu = document.getElementById('nav-menu')
-    menu.classList.remove('d-none');
+      menu.classList.remove('d-none');
     menu.style.right = '0';
-    menu.style.transition = '.8s'
      
 });
 const closeMenu = document.getElementById('close-menu').addEventListener('click', function(){
     let menu = document.getElementById('nav-menu')
-    menu.classList.add('d-none');
+      menu.classList.add('d-none');
     menu.style.right = '-100vw';
-    menu.style.transition = '.8s'
     
 });
+
+window.addEventListener('scroll', function(){
+    const header = this.document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
