7576ca8b921b60c8fcff95668d941dbbddb62760
MasumaMunni <masumamunni20@gmail.com>
2022-09-09 23:42:28 +0600

    menu-updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,23 @@
 
-const openMenu = document.getElementById('open-menu').addEventListener('click', function(){
-    let menu = document.getElementById('nav-menu')
-      menu.classList.remove('d-none');
-    menu.style.right = '0';
+// const openMenu = document.getElementById('open-menu').addEventListener('click', function(){
+//     let menu = document.getElementById('nav-menu')
+//       // menu.classList.remove('d-none');
+//     menu.style.right = '0';
      
-});
-const closeMenu = document.getElementById('close-menu').addEventListener('click', function(){
-    let menu = document.getElementById('nav-menu')
-      menu.classList.add('d-none');
-    menu.style.right = '-100vw';
+// });
+// const closeMenu = document.getElementById('close-menu').addEventListener('click', function(){
+//     let menu = document.getElementById('nav-menu')
+//       // menu.classList.add('d-none');
+//     menu.style.right = '-100vw';
     
-});
+// });
+let menu = document.getElementById('nav-menu')
+function closeMenu(){
+  menu.style.right = '-100%'
+}
+function openMenu(){
+  menu.style.right = '0'
+}
 
 window.addEventListener('scroll', function(){
     const header = this.document.querySelector('header');
