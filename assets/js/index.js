13f403af25fbf12af1c9919cc766df16f2d40e38
MasumaMunni <masumamunni20@gmail.com>
2022-09-13 00:26:02 +0600

    places-added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,4 @@
 
-// const openMenu = document.getElementById('open-menu').addEventListener('click', function(){
-//     let menu = document.getElementById('nav-menu')
-//       // menu.classList.remove('d-none');
-//     menu.style.right = '0';
-     
-// });
-// const closeMenu = document.getElementById('close-menu').addEventListener('click', function(){
-//     let menu = document.getElementById('nav-menu')
-//       // menu.classList.add('d-none');
-//     menu.style.right = '-100vw';
-    
-// });
 let menu = document.getElementById('nav-menu')
 function closeMenu(){
   menu.style.right = '-100%'
@@ -22,4 +10,6 @@ function openMenu(){
 window.addEventListener('scroll', function(){
     const header = this.document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
-})
+});
+
+
